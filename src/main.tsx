19ea24f6b8dfe7570a77fad1d68f81167ecbaf12
gayoung4000-blip <gayoung4000@gmail.com>
2026-07-10 17:02:05 +0900

    feat: 가영 온보딩

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import OnboardingApp from './components/OnboardingApp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <OnboardingApp />
   </StrictMode>,
 )
